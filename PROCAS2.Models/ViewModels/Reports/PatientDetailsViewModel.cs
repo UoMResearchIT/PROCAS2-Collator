7f1f11b160502e800e3f8e6d9619518593da3f60
ajerrison-manchester <andrew.jerrison@manchester.ac.uk>
2018-01-05 16:54:24 +0000

    Start of reports

---
+++ PROCAS2.Models/ViewModels/Reports/PatientDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+using PROCAS2.Resources;
+
+namespace PROCAS2.Models.ViewModels.Reports
+{
+    public class PatientDetailsViewModel
+    {
+        [Display(Name="NHS_NUMBER", ResourceType = typeof(ReportResources))]
+        public string NHSNumber { get; set; }
+    }
+}
---
+++ PROCAS2/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using PROCAS2.Models.ViewModels.Reports;
+
+namespace PROCAS2.Controllers
+{
+#if RELEASE
+    [RequireHttps]
+#endif
+    [Authorize]
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult PatientDetails()
+        {
+            PatientDetailsViewModel model = new PatientDetailsViewModel();
+
+            return View("PatientDetails", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PatientDetails(PatientDetailsViewModel model)
+        {
+
+            return View("PatientDetails", model);
+        }
+    }
+}
